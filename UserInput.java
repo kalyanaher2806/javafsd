7ae54c30a6c039e5ae3d1f5500e5642a421c182a
kalyanaher2806 <101203867+kalyanaher2806@users.noreply.github.com>
2023-11-28 13:11:31 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class UserInput {
+
+	public static void main(String[] args)
+	{
+		int intlput;
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("please enter the interger:");
+		intlput=sc.nextInt();
+		System.out.println("your type"+intlput);
+		
+		float floatlput;
+		System.out.println("please enter the interger:");
+		floatlput=sc.nextFloat();
+		System.out.println("your type"+floatlput);
+		
+		
+		
+	}
+
+}
